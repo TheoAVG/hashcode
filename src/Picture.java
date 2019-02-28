68785db63230f5c1a7b4f56cedbbc8c2b92afda4
theoavg <teofil700@gmail.com>
2019-02-28 21:19:46 +0200

    Version for test of Reader Class

--- src/Picture.java
+++ src/Picture.java
@@ -1,10 +1,13 @@
 public class Picture
 {
-    int type;
+    char type;
     int len;
     String tags;
     public Picture(String data)
     {
         String[] p = data.split(" ");
+        this.type = p[0].charAt(0);
+        this.len = Integer.parseInt(p[1]);
+        this.tags = p[2];
     }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -3,23 +3,27 @@ import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.Scanner;
 class Reader
 {
     File file;
     FileReader reader;
+    Scanner scan;
     List<Picture> data;
     public Reader(String filename) throws IOException
     {    
         this.file = new File(filename);
         this.reader = new FileReader(file);
+        this.scan = new Scanner(reader);
     }
     public List<Picture> read() throws IOException
     {
-        int len = this.reader.read();
+        int len = this.scan.nextInt();
         this.data = new ArrayList<Picture>(len);
         for(int i = 0;i < len;i++)
         {
-            System.out.println("Not yet there");    
+            Picture picture = new Picture(this.scan.nextLine());
+            this.data.add(picture);
         }
         return this.data;
     }
