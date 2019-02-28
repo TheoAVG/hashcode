45f2f3a887df7507362b2b962c889ec1f09e27bd
adrian.batrin <adrian.batrin@ligaac.ro>
2019-02-28 22:00:42 +0200

    Added .out to gitignore

--- src/Compare.java
+++ src/Compare.java
@@ -1,20 +1,31 @@
 import java.util.HashSet;
 
+import static java.lang.Math.min;
+
 public class Compare {
 
     public int compare(String[] a, String[] b) {
         int side_a = 0;
         int common = 0;
-        int size_b = 0;
+        int side_b = 0;
         HashSet<String> hash = new HashSet<String>();
         for(String tag : a) {
             hash.add(tag);
         }
         for(String tag : b) {
-            if(hash.find(tag))
+            if(hash.contains(tag))
                 common++;
-            size_b++;
+            else
+                side_b++;
         }
-        return 5;
+        hash.clear();
+        for(String tag : b) {
+            hash.add(tag);
+        }
+        for(String tag : a) {
+            if(!hash.contains(tag))
+                side_a++;
+        }
+        return Math.min(Math.min(side_a, side_b), common);
     }
 }
